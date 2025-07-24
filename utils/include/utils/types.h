8b607acf3a51f3fd876cfd051321c5d0bdba1c55
Aniket Kumar Roy <aniketroy769@gmail.com>
2025-07-24 23:18:17 +0530

    added byte and address types

--- utils/include/utils/types.h
+++ utils/include/utils/types.h
@@ -9,6 +9,9 @@ namespace xr5 {
 namespace types {
 
 typedef uint64_t Tick;
+typedef uint64_t Address64;
+typedef uint32_t Address32;
+typedef uint8_t Byte;
 typedef double TimeStamp;
 
 } // namespace types
