6c239afc5ed7758ad09ad1af42925f59e28d9d21
Aniket Kumar Roy <aniketroy769@gmail.com>
2025-09-12 08:30:20 +0530

    remove redundant files

--- config/xr5/config.h
+++
@@ -1,6 +0,0 @@
-#ifndef CONFIG_H_
-#define CONFIG_H_
-
-#define XR5_ADDR_WIDTH 64
-
-#endif
--- utils/src/numbers.cc
+++
@@ -1,6 +0,0 @@
-#include "xr5/utils/numbers.h"
-#include <cctype>
-
-namespace xr5 {
-namespace utils {} // namespace utils
-} // namespace xr5
